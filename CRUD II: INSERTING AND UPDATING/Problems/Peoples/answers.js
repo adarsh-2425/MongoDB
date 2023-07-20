cd6a114e0273e1f9e2fb62fa70c856d2ad422a61
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-20 13:44:23 +0530

    Create answers.js

---
+++ CRUD II: INSERTING AND UPDATING/Problems/Peoples/answers.js
@@ -0,0 +1,25 @@
+//question number and description is on README file
+
+1. db.collection.insertOne({
+  name: "Alice",
+  age: 27,
+  email: "alice@example.com",
+  hobbies: ["reading", "cooking"]
+})
+
+2. db.collection.insertMany([
+  {
+    name: "Jack",
+    age: 33,
+    email: "jack@example.com",
+    hobbies: ["running", "swimming"]
+  },
+  {
+    name: "Kate",
+    age: 29,
+    email: "kate@example.com",
+    hobbies: ["travelling", "hiking"]
+  }
+])
+
+3. 
